44c23491263482cfc0718a89e78bf4ca8f43fb2a
Nikhil Chauhan <5293647+niikhill@users.noreply.github.com>
2018-01-03 22:32:42 +0530

    Update midoscript.sh

--- midoscript.sh
+++ midoscript.sh
@@ -12,6 +12,6 @@ git clone https://github.com/Adesh15/hardware_qcom_display -b lineage-15.0-caf-8
 
 git clone https://github.com/Adesh15/android_kernel_xiaomi_msm8953.git -b featherO_rebase kernel/xiaomi/msm8953
 
-git clone https://github.com/CaptRosh/vendor_xiaomi_mido.git -b 8.1 vendor/xiaomi/mido
+git clone https://github.com/CaptRosh/vendor_xiaomi_mido.git -b 8.1 vendor/xiaomi/
 
 git clone https://github.com/niikhill/device_xiaomi_mido.git -b oreo-mr1 device/xiaomi/mido
