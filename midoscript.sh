435265303369f1249beba0d36a2bfea49308eab2
Nikhil Chauhan <5293647+niikhill@users.noreply.github.com>
2018-01-12 09:59:35 +0530

    Update midoscript.sh
    tracking my own kernel tree

--- midoscript.sh
+++ midoscript.sh
@@ -10,8 +10,8 @@ git clone https://github.com/Adesh15/hardware_qcom_audio -b lineage-15.0-caf-899
 
 git clone https://github.com/Adesh15/hardware_qcom_display -b lineage-15.0-caf-8996 hardware/qcom/display-caf/msm8937
 
-git clone https://github.com/Adesh15/android_kernel_xiaomi_msm8953.git -b featherO_rebase kernel/xiaomi/msm8953
+git clone https://github.com/niikhill/android_kernel_xiaomi_msm8953 -b featherO_rebase kernel/xiaomi/msm8953
 
-git clone https://github.com/CaptRosh/vendor_xiaomi_mido.git -b 8.1 vendor/xiaomi
+git clone https://github.com/devstark15/proprietary_vendor_xiaomi.git -b oreo-mr1 vendor/xiaomi
 
-git clone https://github.com/niikhill/device_xiaomi_mido.git -b oreo-mr1 device/xiaomi/mido
+git clone https://github.com/niikhill/device_xiaomi_mido.git -b du device/xiaomi/mido
